fa349d5c34dd3b99589089a8284918938feded09
Icosahunter <anzelknight@gmail.com>
2023-01-01 14:06:44 -0600

    Work on install command

--- cli.py
+++ cli.py
@@ -3,7 +3,7 @@ import common as omni
 
 parser = argparse.ArgumentParser(
     prog = 'omnipkg',
-    description = 'A simple GUI for interfacing with multiple package management systems on Linux'
+    description = 'A simple program for interfacing with multiple package management systems on Linux'
 )
 
 subparsers = parser.add_subparsers()
@@ -12,9 +12,23 @@ def search(args):
     for line in omni.search(args.package):
         print(line)
 
+def install(args):
+    print(omni.install(args.package))
+
+def uninstall(args):
+    omni.uninstall(args.package)
+
 search_parser = subparsers.add_parser('search')
 search_parser.add_argument('package')
 search_parser.set_defaults(func=search)
+
+search_parser = subparsers.add_parser('install')
+search_parser.add_argument('package')
+search_parser.set_defaults(func=install)
+
+search_parser = subparsers.add_parser('uninstall')
+search_parser.add_argument('package')
+search_parser.set_defaults(func=uninstall)
 
 args = parser.parse_args()
 args.func(args)
--- common.py
+++ common.py
@@ -10,17 +10,29 @@ for path in os.listdir('./pm-defs'):
     with open('./pm-defs/' + path) as f:
         pkg_managers.append(json.load(f))
 
-def run(cmd, pkg=None):
+def run(cmd, pkg=None, capture=False):
     command = cmd
     if pkg != None:
         command = command.format(package=pkg)
-    result = subprocess.run(shlex.split(command), capture_output=True)
-    return result.stdout.decode('ascii')
-
+    result = subprocess.run(shlex.split(command), capture_output=capture)
+    if capture:
+        return result.stdout.decode('ascii')
+    else:
+        return result.returncode
+    
 def search(package):
     results = []
     for pm in pkg_managers:
-        stdout = run(pm['search'][0], package)
-        regex = re.compile(pm['search'][1])
-        results.extend((regex.search(line).group(0) for line in stdout.split('\n')))
+        results.extend(search_in_pm(package, pm))
     return results
+
+def search_in_pm(package, pm):
+    stdout = run(pm['search'][0], package, True)
+    regex = re.compile(pm['search'][1])
+    return ((regex.search(line).group(0) for line in stdout.split('\n')))
+
+def install(package):
+    for pm in pkg_managers:
+        if package in search_in_pm(package, pm):
+            return run(pm['install'], package)
+    return f'Package "{package}" not found in any repository.'
